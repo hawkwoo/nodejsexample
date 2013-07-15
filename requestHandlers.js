660c0dd1304d3b427073d7f6ef088985b705aeb8
hawkwoo <wp11111111@126.com>
2013-07-15 15:09:23 +0000

    requesthandlers.js

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,6 @@
 var querystring =require("querystring");
 var fs=require("fs");
+var formidable = require("formidable");
 
 function start(response, postData){
     console.log("Request handler 'start' was called.");
@@ -22,12 +23,23 @@ function start(response, postData){
 
 function upload(response, postData){
     console.log("Request handler 'upload' was called.");
-    response.writeHead(200,{"Content-Type":"text/plain"});
-    response.write("You 've set the text: "+querystring.parse(postData).text);
+    var form=new formidable.IncomingForm();
+    console.log("about to parse");
+    form.parse(request, function(error, fields, files){
+	console.log("parsing done");
+	fs.rename(files.upload.path,"/tmp/test.png",function(error){
+	    if(error){
+		fs.unlink("/tmp/test.png");
+		fs.rename(files.upload.path,"/tmp/test.png");
+		}
+	);
+    response.writeHead(200,{"Content-Type":"text/html"});
+    response.write("received image:<br/>");
+    response.write("<img src='/show' />");		  
     response.end();
 }
 
-function show(response, postData){
+function show(response){
     console.log("Request handler 'show' was called.");
     fs.readFile("/tmp/test.png","binary",function(error,file){
 	if(error){
