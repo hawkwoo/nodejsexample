fc7f1bdaf91550130f428dcc0992de700a29bbfa
hawkwoo <wp11111111@126.com>
2013-07-15 11:57:57 +0000

    server.js start edition

--- server.js
+++ server.js
@@ -1,24 +1,16 @@
-var formidable =require('formidable');
 var http=require("http");
 var url =require("url");
-var sys=require('sys');
 
-http.createServer(function(req,res){
-    if(req.url=='/upload' && req.method.toLowerCase() =='post'){
-	var form =new formidable.IncomingForm();
-	form.parse(req,function(error,fields,files){
-	    res.writeHead(200,{'content-type': 'text/plain'});
-	    res.write('received upload:\n\n');
-	    res.end(sys.inspect({fields:fields, files:files}));
-	   }
-	);
-	return;
+function start(route, handle){
+    function onRequest(request , response){
+	var pathname=url.parse(request.url).pathname;
+	console.log("Request for "+ pathname+ "received.");
+	route(handle, pathname, response, request);
     }
+    
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started.");
+}
 
-    res.writeHead(200,{'content-type':'text/html'});
-    res.end(
-	'<form action="/upload" enctype="multipart/form-data"'+'method="post">'+'<input type="text" name="title"><br>'+
-'<input type="file" name="upload" multiple="multiple"><br>'+
-'<input type="submit" value="Upload">'+'</form>'
-);
-}).listen(8888);
+exports.start=start;
+
